2e311c9d31869563e0a06e7f341cb0f6edda8159
Administrator <Administrator@DESKTOP-GARLJAI>
2024-05-02 09:46:53 -0800

    First commit

---
+++ SeleniumProjectDemo/src/com/SeleniumDemo/GooglePage.java
@@ -0,0 +1,22 @@
+package com.SeleniumDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GooglePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "F:\\Selenium JAR file\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
+
+		WebDriver driver=new ChromeDriver(); //Creating object of WebDriver Interface.
+
+		driver.manage().window().maximize();
+
+		driver.get("https://www.google.com/");
+
+		driver.close();
+
+	}
+
+}
